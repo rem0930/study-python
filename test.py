a058e57626cced85df6b8d5817458d06bba94cb7
rem0930 <06araki@gmail.com>
2021-05-15 10:40:22 +0900

    name

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("arki")
+# coding:utf-8
+print("こんにちは。今日の晩ご飯はなんでしたか？ \n おいしかったですか？ \n 何カロリーでしたか？")
+
+a=1
+b=2
+c="abc"
+
+username="rem"
+a
+print(username)
+print(a+b)
+
+for a in [1,2,3,4,5]:
+    print(a)
